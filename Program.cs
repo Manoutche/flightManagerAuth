5b1e1327d32926cc9de995164291a0a15a6b6a30
Manoutche <67541888+Manoutche@users.noreply.github.com>
2025-01-24 07:59:51 +0000

    include Angular app

--- Program.cs
+++ Program.cs
@@ -1,5 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+// Ajouter une politique CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200") // L'origine de votre application Angular
+              .AllowAnyHeader() // Autoriser tous les en-têtes HTTP
+              .AllowAnyMethod(); // Autoriser toutes les méthodes HTTP (GET, POST, etc.)
+    });
+});
 builder.Services.AddControllers();
 var app = builder.Build();
+// Utiliser la politique CORS
+app.UseCors("AllowAngularApp");
 app.MapControllers();
 app.Run();
